44a38229d2174226835106d0fb1349faaafe4178
TiagoCastilhos <tiagocastilhos66@gmail.com>
2020-10-05 19:07:01 -0300

    implemented console functionalities

--- src/Calculadora/Program.cs
+++ src/Calculadora/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Calculadora.Exceptions;
 
 namespace Calculadora
 {
@@ -6,7 +7,88 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var resposta = Menu();
+            var calculadora = new Calculadora();
+
+            switch (resposta)
+            {
+                case 1:
+                    Soma(calculadora);
+                    break;
+                case 2:
+                    Subtracao(calculadora);
+                    break;
+                case 3:
+                    Multiplicacao(calculadora);
+                    break;
+                case 4:
+                    Divisao(calculadora);
+                    break;
+            }
+
+            Console.Read();
+        }
+
+        static int Menu()
+        {
+            Console.WriteLine("Escolha uma operacao: ");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtracao");
+            Console.WriteLine("3 - Multiplicacao");
+            Console.WriteLine("4 - Divisao");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void Soma(Calculadora calculadora)
+        {
+            Console.WriteLine("Escolha o primeiro numero:");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha o segundo numero:");
+            var b = int.Parse(Console.ReadLine());
+            var resultado = calculadora.Soma(a, b);
+
+            Console.WriteLine($"Resultado da soma: {resultado}");
+        }
+
+        static void Subtracao(Calculadora calculadora)
+        {
+            Console.WriteLine("Escolha o primeiro numero:");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha o segundo numero:");
+            var b = int.Parse(Console.ReadLine());
+            var resultado = calculadora.Subtração(a, b);
+
+            Console.WriteLine($"Resultado da subtracao: {resultado}");
+        }
+
+        static void Multiplicacao(Calculadora calculadora)
+        {
+            Console.WriteLine("Escolha o primeiro numero:");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha o segundo numero:");
+            var b = int.Parse(Console.ReadLine());
+            var resultado = calculadora.Multiplicacao(a, b);
+
+            Console.WriteLine($"Resultado da multiplicacao: {resultado}");
+        }
+
+        static void Divisao(Calculadora calculadora)
+        {
+            Console.WriteLine("Escolha o primeiro numero:");
+            var a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escolha o segundo numero:");
+            var b = int.Parse(Console.ReadLine());
+
+            try
+            {
+                var resultado = calculadora.Divisao(a, b);
+
+                Console.WriteLine($"Resultado da divisao: {resultado}");
+            }
+            catch (DivisaoPorZeroException)
+            {
+                Console.WriteLine("Divisor nao pode ser 0");
+            }
         }
     }
 }
